b301f6ddbcc4e82a861e7e6b5e99f6cb450620bd
Leonard K. Fiadzinu <leo@gmgltd.com>
2016-08-09 00:25:36 -0400

    added more filter

--- bin/start-parity.sh
+++ bin/start-parity.sh
@@ -6,6 +6,7 @@ root=$HOME
 # chainBlock="testnet"
 dataDir="paridata"
 
+
 # if [ ! $# == 1 ]; then
 if [ $# -lt 1 -o $# -ge 2 ]; then
     echo "Use need to add one parimeter to the command"
@@ -14,6 +15,27 @@ if [ $# -lt 1 -o $# -ge 2 ]; then
     echo "e.g npm start livenet"
     exit
 fi
+
+# so we have installed parity and rust
+# befor we go any deeper lets check if we have at least one account.
+
+output=( $(parity account list) )
+coinbase=${output[2]}
+
+if [ ! -z "$coinbase" ]; then
+    address=${output[2]}
+    # exit
+else
+    $(parity account new --password ./lib/pswd)
+    output=( $(parity account list) )
+    address=${output[2]}
+    echo "Since we couldn't find an address in parity for you, so we took the time to create one account. The password is located in the lib/pswd file"
+    echo " "
+    echo "here it is : " $address " you can also use parity account list, to show all your accounts"
+    # exit
+fi
+
+
 
 # echo $#
 
@@ -47,7 +69,7 @@ echo "trying to start this in a $typeofchain, you should be seeing synic files s
 if [ $typeofchain = "private" ]; then
 
 # address=0x$(parity account list | awk 'END{print}' | tr -cd '[[:alnum:]]._-')
-address=0x$(parity account list | awk 'BEGIN{print}' | tr -cd '[[:alnum:]]._-')
+# address=0x$(parity account list | awk 'BEGIN{print}' | tr -cd '[[:alnum:]]._-')
 
 echo "address: $address"
 echo ""
@@ -56,15 +78,15 @@ echo "HENCE YOU WILL NEED TO START ETHMINER WITH THE COMMAND IN ANOTHER TERMINAL
 echo "open a new terminal ctrl + alt + t, and type ethminer"
 
 # parity --chain ./lib/genesis.json  --author ${address} --unlock ${address} --password ./lib/pswd --jsonrpc-interface all
-# parity --identity leonode --datadir $root/$dataDir/$typeofchain $chainBlock  --author 8c8091a36a79f28f4fb8c85ae00d5a2ee1da0e12
-# parity $chainBlock --identity leonode --nodiscover --nat none --datadir $root/$dataDir/$typeofchain  $chainBlock --pruning fast --author 8c8091a36a79f28f4fb8c85ae00d5a2ee1da0e12 -jw ui
-parity --chain $root/parity-eth/lib/genesis.json --datadir $root/$dataDir/$typeofchain  --keys-path $root/$dataDir/$typeofchain/keys  --unlock 8c8091a36a79f28f4fb8c85ae00d5a2ee1da0e12  --password ./lib/pswd  --identity sonia  --pruning fast --author 8c8091a36a79f28f4fb8c85ae00d5a2ee1da0e12 ui
+# parity --identity leonode --datadir $root/$dataDir/$typeofchain $chainBlock  --author $address
+# parity $chainBlock --identity leonode --nodiscover --nat none --datadir $root/$dataDir/$typeofchain  $chainBlock --pruning fast --author $address -jw ui
+parity --chain $root/parity-eth/lib/genesis.json --datadir $root/$dataDir/$typeofchain  --keys-path $root/$dataDir/$typeofchain/keys  --unlock $address  --password ./lib/pswd  --identity sonia  --pruning fast --author $address ui
 # parity --chain ~/parity-eth/lib/genesis.json --fork normal --datadir $root/$dataDir/$typeofchain --pruning fast -j ui
 echo "parity --chain $root/parity-eth/lib/genesis.json --datadir $root/paridata/private -jw ui"
 
 else
 
 # parity --datadir=$root/$dataDir/$typeofchain  --chain=$chainBlock --unlock="" --pruning=fast --author= ui
-parity --datadir $root/$dataDir/$typeofchain $chainBlock --keys-path $root/$dataDir/$typeofchain/keys --unlock 8c8091a36a79f28f4fb8c85ae00d5a2ee1da0e12  --password ./lib/pswd  --pruning fast --author 8c8091a36a79f28f4fb8c85ae00d5a2ee1da0e12  ui
+parity --datadir $root/$dataDir/$typeofchain $chainBlock --keys-path $root/$dataDir/$typeofchain/keys --unlock $address --password ./lib/pswd --pruning fast --author $address ui
 
 fi
