4f72d2da842bd1825dce6dabd2666ad727ada0a4
Leonard K. Fiadzinu <leo@gmgltd.com>
2016-08-09 20:25:55 -0400

    this file should help in upgrade

--- bin/upgrade-parity.sh
+++ bin/upgrade-parity.sh
@@ -6,3 +6,29 @@ function pause(){
     read -p "$*"
 }
 
+parityVersion=( $(parity --version) )
+currentParityVer=${parityVersion[2]}
+
+pause "The upgrade will only be done for npm packages and parity, your current parity version is $currentParityVer, press [Enter] to continue..."
+echo ""
+echo $currentParityVer
+
+# now we run the script to install parity, as it doesn't have an upgrade command yet'
+# default easy install command, but it is behind
+#bash <(curl https://get.parity.io -Lk) # this one install parity without the rust compiler for development
+
+# the command below get you the latest working copy but you have to do extra working.
+# once this is build you will have to copy files to /usr/local/bin
+# nevermind it is taken care of....
+# bash <(curl https://raw.githubusercontent.com/ethcore/parity/master/install-deps.sh -L)
+
+# cd /tmp/parity/ && cp target/
+
+parityUpgradeVersion( $(parity --version) )
+upgradedParityVer=${parityUpgradeVersion[2]}
+
+if [ $currentParityVer = $upgradedParityVer ]; then
+echo "it looks like the current parity is the same as the upgrade"
+else
+echo "Your new parity version is $upgradedParityVer"
+fi
