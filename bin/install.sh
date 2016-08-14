b665bb01b7b71cf2b8e49f02217d20e01a7535a6
Leonard K. Fiadzinu <leo@gmgltd.com>
2016-08-14 16:09:36 -0400

    updated install script

--- bin/install.sh
+++ bin/install.sh
@@ -16,15 +16,17 @@ sudo add-apt-repository -y ppa:ethereum/ethereum
 sudo add-apt-repository -y ppa:ethereum/ethereum-dev
 sudo apt-get -y update
 sudo apt-get -y upgrade
-sudo apt-get install solc ethminer -y
-
+#sudo apt-get install ethminer -y
+#sudo apt-get install solc -y
 pause 'This script will install most of the Ethereum parity packages, press [Enter] to continue...'
 
 RUSTC_PATH=$(which rustc 2>/dev/null)
 if [[ -f $RUSTC_PATH ]]; then
     echo " rustc is already installed... moving along"
 else
-    bash <(curl https://sh.rustup.rs -sSf | sh)
+   # bash <(curl https://sh.rustup.rs -sSf | sh)
+   curl https://sh.rustup.rs -sSf | sh
+   source $HOME/.cargo/env
 fi
 
 # this is for the binary install of parity don't need to do anythiny else one it is done.
@@ -41,6 +43,16 @@ pause 'hopefully no errors showed up in the install process, press [Enter] to fi
 echo "npm start private for a private blockchain"
 echo "npm start testnet for a testnet blockchain"
 echo "npm start livenet for a live net blockchain"
+echo ""
+parityVersion=( $(parity --version) )
+currentParityVer=${parityVersion[2]}
+
+parityPath=$(which parity 2>/dev/null)
+if [[ -f $parityPath ]]; then
+    echo "We are goood to goooooooo: $currentParityVer"
+else
+    echo "something really went wrong"
+fi
 # uncomment this out if you are running the developer install.
 # echo "Run the commands below"
 # print "cd /tmp/parity/ and cp"
