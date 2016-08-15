fd68104b3e1866f806079ed0c6711f0f25a3de62
Leonard K. Fiadzinu <leo@gmgltd.com>
2016-08-15 17:31:48 -0400

    testing the create script

--- bin/start-parity.sh
+++ bin/start-parity.sh
@@ -37,7 +37,10 @@ if [ $output = "[]" ]; then
     echo "Since we couldn't find an address in parity for you, we took the time to create one account. The password is located in the lib/pswd file"
     echo " "
 
-   $(parity --password ./lib/pswd account new)
+    echo $(parity account new)
+    read PSWD
+    echo $PSWD
+    exit 1
     output=( $(parity account list) )
     coinbase=${output[0]}
     coinbase1=${coinbase#"["}
